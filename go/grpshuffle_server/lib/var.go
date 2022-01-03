9333df4b96e64ecd36921b6fdfc62d774d752e92
Futa HIRAKOBA <futa-hirakoba@cybozu.co.jp>
2022-01-03 22:07:11 +0900

    feat (go): support change port for server

---
+++ go/grpshuffle_server/lib/var.go
@@ -0,0 +1,7 @@
+package grpshuffle_server
+
+var (
+	Port             int  // Port is port of grpshuffle server
+	PrometheusEnable bool // PrometheusEnable  indicates whether to use Prometheus
+	PrometheusPort   int  // PrometheusPort is port of Prometheus
+)
--- go/grpshuffle_server/main.go
+++ go/grpshuffle_server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	grpshuffleServer "github.com/korosuke613/grpshuffle/go/grpshuffle_server/lib"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -13,14 +13,14 @@ import (
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/korosuke613/grpshuffle/go/grpshuffle"
+	grpshuffleServer "github.com/korosuke613/grpshuffle/go/grpshuffle_server/lib"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	health "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/keepalive"
 )
-
-const portNumber = 13333
 
 func extractFields(fullMethod string, req interface{}) map[string]interface{} {
 	ret := make(map[string]interface{})
@@ -37,49 +37,90 @@ func extractFields(fullMethod string, req interface{}) map[string]interface{} {
 }
 
 func main() {
-	kep := keepalive.EnforcementPolicy{
-		MinTime: 60 * time.Second,
+	app := &cli.App{
+		Name:  "grpshuffle-server",
+		Usage: "Server of groshuffle",
+		Action: func(c *cli.Context) error {
+			kep := keepalive.EnforcementPolicy{
+				MinTime: 60 * time.Second,
+			}
+
+			logger, _ := zap.NewProduction()
+			serv := grpc.NewServer(
+				grpc.KeepaliveEnforcementPolicy(kep),
+				grpc.StreamInterceptor(
+					grpc_middleware.ChainStreamServer(
+						grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(extractFields)),
+						grpc_zap.StreamServerInterceptor(logger),
+						grpc_prometheus.StreamServerInterceptor,
+					),
+				),
+				grpc.UnaryInterceptor(
+					grpc_middleware.ChainUnaryServer(
+						grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(extractFields)),
+						grpc_zap.UnaryServerInterceptor(logger),
+						grpc_prometheus.UnaryServerInterceptor,
+					),
+				),
+			)
+
+			grpshuffle.RegisterComputeServer(serv, &grpshuffleServer.Server{})
+			health.RegisterHealthServer(serv, &grpshuffleServer.HealthServer{})
+			grpc_prometheus.Register(serv)
+
+			fmt.Println(grpshuffleServer.PrometheusEnable)
+			if grpshuffleServer.PrometheusEnable {
+				http.Handle("/metrics", promhttp.Handler())
+				prometheusAddr := fmt.Sprintf(":%v", grpshuffleServer.PrometheusPort)
+				go func() {
+					log.Printf("Launch Prometheus server %v...", grpshuffleServer.PrometheusPort)
+					if err := http.ListenAndServe(prometheusAddr, nil); err != nil {
+						panic(err)
+					}
+				}()
+			}
+
+			log.Printf("Launch grpshuffle server %v...", grpshuffleServer.Port)
+			l, err := net.Listen("tcp", fmt.Sprintf(":%d", grpshuffleServer.Port))
+			if err != nil {
+				return fmt.Errorf("failed to listen:%v", err)
+			}
+
+			err = serv.Serve(l)
+			if err != nil {
+				return fmt.Errorf("failed to serve:%v", err)
+			}
+
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "port",
+				Aliases:     []string{"P"},
+				Usage:       "`PORT` of server",
+				EnvVars:     []string{"GRPSHUFFLE_PORT"},
+				Value:       13333,
+				Destination: &grpshuffleServer.Port,
+			},
+			&cli.BoolFlag{
+				Name:        "prometheus-enable",
+				Usage:       "With this option, serve Prometheus",
+				EnvVars:     []string{"GRPSHUFFLE_PROMETHEUS_ENABLE"},
+				Value:       false,
+				Destination: &grpshuffleServer.PrometheusEnable,
+			},
+			&cli.IntFlag{
+				Name:        "prometheus-port",
+				Usage:       "`PORT` of prometheus",
+				EnvVars:     []string{"GRPSHUFFLE_PROMETHEUS_PORT"},
+				Value:       8081,
+				Destination: &grpshuffleServer.PrometheusPort,
+			},
+		},
 	}
 
-	log, _ := zap.NewProduction()
-	serv := grpc.NewServer(
-		grpc.KeepaliveEnforcementPolicy(kep),
-		grpc.StreamInterceptor(
-			grpc_middleware.ChainStreamServer(
-				grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(extractFields)),
-				grpc_zap.StreamServerInterceptor(log),
-				grpc_prometheus.StreamServerInterceptor,
-			),
-		),
-		grpc.UnaryInterceptor(
-			grpc_middleware.ChainUnaryServer(
-				grpc_ctxtags.UnaryServerInterceptor(grpc_ctxtags.WithFieldExtractor(extractFields)),
-				grpc_zap.UnaryServerInterceptor(log),
-				grpc_prometheus.UnaryServerInterceptor,
-			),
-		),
-	)
-
-	grpshuffle.RegisterComputeServer(serv, &grpshuffleServer.Server{})
-	health.RegisterHealthServer(serv, &grpshuffleServer.HealthServer{})
-	grpc_prometheus.Register(serv)
-
-	http.Handle("/metrics", promhttp.Handler())
-	go func() {
-		if err := http.ListenAndServe(":8081", nil); err != nil {
-			panic(err)
-		}
-	}()
-
-	l, err := net.Listen("tcp", fmt.Sprintf(":%d", portNumber))
+	err := app.Run(os.Args)
 	if err != nil {
-		fmt.Println("failed to listen:", err)
-		os.Exit(1)
-	}
-
-	err = serv.Serve(l)
-	if err != nil {
-		fmt.Println("failed to serve:", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
